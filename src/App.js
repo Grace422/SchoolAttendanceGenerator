19f59f1856490bf0bb688758703fb6b9e6b2358c
Grace <neguekwahammaelgrace@gmail.com>
2025-01-16 17:48:37 +0100

    download function added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import './App.css';
 import InputForm from './InputForm'; 
-import SchoolForm from './SchoolForm';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={ <InputForm/> }/>
-        <Route path ='/school' element={ <SchoolForm/> }/>
-        <Route path='/input' element={ <InputForm/> }/>
-      </Routes>
-    </BrowserRouter>
+    <InputForm/>
   );
 }
 
--- src/InputForm.js
+++ src/InputForm.js
@@ -1,67 +1,162 @@
-import React, { useState } from 'react';
-import html2pdf from 'html2pdf.js'
-import { Link } from 'react-router-dom';
+import React, { useState, useRef } from 'react';
+import html2pdf from 'html2pdf.js/dist/html2pdf.min.js'
+import img from './images/ub-logo.jpeg';
+
+
 
 export default function InputForm() {
-    const [input, setInput] = useState('')
-    const [isPreviewed, setIsPreviewed] = useState(false)
-    const [name, setName] = useState()
-    const [mat, setMat] = useState()
+    const [file, setFile] = useState()
+    const fileReader = new FileReader()
+    const handleOnChange = (e) => {
+        setFile(e.target.files[0]);
+    }
+    const [formData, setFormData] = useState({
+        name: '',
+        matricule: '',
+        department: '',
+        level: '',
+    });
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }))
+    }
+    const componentRef = useRef()
+    const handleDownload = () => {
+        const element = componentRef.current
+        console.log(componentRef.current);
+        const opt = {
+            margin: 1,
+            filename: `${formData.name}_Form.pdf`,
+            image: { type: 'png', quality: 0.99, compression: 'best' },
+            html2canvas: { scale: 3 },
+            jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
+            output: 'blob'
+        }
+        html2pdf().set(opt).from(element).save()
+    }
+    const currentDate = new Date()
+    const options = {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric',
+    };
+    const levalMapping = {
+            '200': 'First year',
+            '300': 'Second year',
+            '400': 'Third year',
+            '500': 'Fourth year',
+    }
+    const formattedDate = currentDate.toLocaleDateString('en-GB', options)
+    
+    // if (!formData.name || !formData.matricule || !formData.department || !formData.level){
+    //     navigate('/')
+    //     alert('Please fill in all required fields before downloading the letter.');
+    //     return null;
+    // }
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        setInput('')
-    }
-    const handleClick = () => {
-        const element = document.querySelector('#attendance')
-        html2pdf(element);
-    }
     return (
+        <main>
         <div className='input'>
             <p>School Attendance</p>
-            <form onSubmit={handleSubmit}>
+            <form>
                 <label>Name:</label>
-                <input type='text' value={name} onChange={(e)=>setName(e.target.value)}/>
+                <input type='text' name='name' value={formData.name} onChange={handleChange} required />
 
                 <label>Matricule:</label>
-                <input type='text' value={mat} onChange={(e)=>setMat(e.target.value)}/>
+                <input type='text' name='matricule' value={formData.matricule} onChange={handleChange} required />
 
                 <div className='select'>
-                    <label>Level:</label>
-                    <select>
+                    <label>Level:</label> <br/>
+                    <select name='level' value={formData.level} onChange={handleChange} required>
+                        <option value=''> </option>
                         <option value='200'>Level 200</option>
                         <option value='300'>Level 300</option>
                         <option value='400'>Level 400</option>
                         <option value='500'>Level 500</option>
                     </select>
-                    <div />
-                    <div className='select'>
-                        <label>Sex:</label>
-                        <select>
-                            <option value='Male'>Male</option>
-                            <option value='Female'>Female</option>
-                        </select>
-                    </div>
+                <div />
 
-                    <div className='select'><label>Deparment:</label>
-                        <select>
-                            <option value='computer'>Computer Engineering</option>
-                            <option value='electrical'>Electrical Engineering</option>
-                            <option value='mechanical'>Mechanical Engineering</option>
-                            <option value='civil'>Civil Engineering</option>
+                <div className='select'>
+                        <label>Deparment:</label>
+                        <select name='department' value={formData.department} onChange={handleChange} required>
+                            <option value=''> </option>
+                            <option value='Computer Engineering'>Computer Engineering</option>
+                            <option value='Electrical Engineering'>Electrical Engineering</option>
+                            <option value='Mechanical Engineering'>Mechanical Engineering</option>
+                            <option value='Civil Engineering'>Civil Engineering</option>
                         </select>
-                    </div>
+                </div>
 
 
                     <div className='input-btn'>
-                        <button onClick={() => setIsPreviewed(true)}>
-                            <Link to='/school' className='preview-link'>Preview</Link>
-                        </button>
-                        <button onClick={handleClick}>Download</button>
+                        <input type={'file'} accept={".csv"} id={"csvFileInput"}/>
+                        <button onClick={handleDownload} type='button'>Download</button>
+                        <button onClick={handleOnChange} type='button'>Download All</button>
                     </div>
                 </div>
-
             </form>
         </div>
+
+        <div className='container' ref={componentRef}>
+                <div className='top'>
+                    <p>
+                        <strong style={{ fontSize: 'larger' }}>UNIVERSITY OF BUEA</strong> <br />
+                        <div style={{ fontSize: '15px', fontFamily: 'arial' }}>
+                            P.O. Box 63, <br />
+                            Buea, South West Region <br />
+                            CAMEROON <br />
+                            Tel: (237) 3332 21 34/3332 26 90/3332 27 06/3332 28 13 <br />
+                            Fax: (237) 3332 22 27
+                        </div>
+                    </p>
+                    <img src={img} alt='ub-logo' className='image' />
+                    <p className='top-left'>
+                        <strong style={{ fontSize: 'larger' }}>REPUBLIC OF CAMEROON</strong> <br />
+                        PEACE-WORK-FATHERLAND
+                    </p>
+                </div>
+                <div className='top-bottom'>
+                    <p>
+                        <strong style={{ fontSize: 'larger', marginBottom: '50px' }}>FACULTY OF ENGINEERING AND TECHNOLOGY</strong> <br /> <br />
+                        Dean: <strong>Agbor Dieudonne Agbor,PhD</strong> <br />
+                        Vice-Dean: <strong>Dr. Nde D.Nguti</strong> <br />
+                        Faculty Officer: <strong>Mme. Ekpombang Patience Taku</strong> <br />
+                    </p>
+                </div>
+                <hr />
+                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <div style={{ marginTop: '15px' }}>
+                        <strong>Your Ref:</strong> <hr className='line' />
+                        <strong>Our Ref: <p className='year'>2024/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/UB/D/FET</p></strong>
+                    </div>
+                    <div>
+                        <p> Date: <strong className='date' id='date'>{formattedDate}</strong> </p>
+                    </div>
+                </div>
+                <br />
+
+                <p className='title'>ATTESTATION OF SCHOOL ATTENDANCE</p>
+                <p className='text'>
+                    I, the undersigned, Dean of the Faculty of Engineering and Technology do, hereby attest that 
+                    <strong> {formData.name} </strong> with matriculation number <strong> {formData.matricule} </strong> is a Level <span>{formData.level}</span> 
+                    {formData.level && (
+                          <span> ({levalMapping[formData.level]}) </span>
+                    )}
+                    student in the Faculty of Engineering and Technology, Department of <span> {formData.department} </span> for the academic year 2023/2024.
+                </p> <br />
+                <p className='text'>
+                    In withness whereof this attestation is issued to serve as and where necessary.
+                </p> <br />
+                <div className='bottom'>
+                    <p>
+                        <strong>Agbor Dieudonne Agbor,PhD</strong> <br />
+                        Dean <br />
+                    </p>
+                </div>
+            </div>
+        </main>
     )
 }
